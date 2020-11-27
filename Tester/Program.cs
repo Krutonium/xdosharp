956358ea5e42b2ecb7ce755dbe53dc05695fe006
Krutonium <PFCKrutonium@gmail.com>
2020-11-27 12:33:35 -0500

    A basic, functional library

---
+++ Tester/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using xdosharp;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Xdo x = new Xdo();
+            x.SendKey("s", "minecraft");
+            x.SendSentence("t spaces don't work yet");
+            x.Click("1.16.4", false);
+        }
+    }
+}
---
+++ xdosharp/Xdo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.Xml;
+
+namespace xdosharp
+{
+    public class Xdo
+    {
+        public void SendKey(string Key, string WindowName = null)
+        {
+            if (WindowName != null)
+            {
+                FocusWindow(WindowName);
+            }
+            DoXDo("key " + Key);
+        }
+
+        public void SendSentence(string Sentence, string WindowName = null)
+        {
+            if (WindowName != null)
+            {
+                FocusWindow(WindowName);
+            }
+
+            foreach (char P in Sentence)
+            {
+                SendKey(P.ToString(), WindowName);
+            }
+        }
+
+        public void Click(string WindowName, bool Right = false )
+        {
+            string Window = "";
+            if (WindowName != null)
+            {
+                Window = FocusWindow(WindowName);
+            }
+            if (Right)
+            {
+                DoXDo("getactivewindow mousemove 0 0 mousemove --window " + Window + " 10 10 click 2 mousemove restore");
+            }
+            else
+            {
+                DoXDo("getactivewindow mousemove 0 0 mousemove --window " + Window + " 10 10 click 1 mousemove restore");
+            }
+        }
+
+        private string FocusWindow(string WindowName)
+        {
+            ProcessStartInfo WID = new ProcessStartInfo();
+                WID.FileName = "xdotool";
+                WID.Arguments = "search --name " + WindowName;
+                WID.UseShellExecute = false;
+                WID.RedirectStandardOutput = true;
+                var P = Process.Start(WID);
+                P.WaitForExit();
+                var Output = P.StandardOutput.ReadLine();
+#if DEBUG
+                Console.WriteLine("ID is " + Output);
+#endif
+                DoXDo("windowactivate --sync " + Output);
+                //System.Threading.Thread.Sleep(100);
+                return Output;
+        }
+
+        private static void DoXDo(string arguments)
+        {
+            ProcessStartInfo XDoTool = new ProcessStartInfo();
+            XDoTool.FileName = "xdotool";
+            XDoTool.Arguments = arguments;
+            Process.Start(XDoTool).WaitForExit();
+        }
+    }
+}
